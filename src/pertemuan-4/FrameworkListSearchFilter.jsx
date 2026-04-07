68fef481d2e70f0614198bc83d90c8ad24da157c
della1206 <della24si@mahasiswa.pcr.ac.id>
2026-04-07 16:31:07 +0700

    Pertemuan 4 Responsive & Grid Design

--- src/pertemuan-4/FrameworkListSearchFilter.jsx
+++ src/pertemuan-4/FrameworkListSearchFilter.jsx
@@ -2,11 +2,28 @@ import { useState } from "react";
 import frameworkData from "./framework.json";
 export default function FrameworkList() { 
 		/** Deklrasai state **/
-		const [searchTerm, setSearchTerm] = useState("");
-		const [selectedTag, setSelectedTag] = useState("");
+		//const [searchTerm, setSearchTerm] = useState("");
+		//const [selectedTag, setSelectedTag] = useState("");
     
+    /*Inisialisasi DataForm*/
+		const [dataForm, setDataForm] = useState({
+			searchTerm: "",
+			selectedTag: "",
+      searchDev:"",
+			/*Tambah state lain beserta default value*/
+			});
+		
+		/*Inisialisasi Handle perubahan nilai input form*/
+		const handleChange = (evt) => {
+			const { name, value } = evt.target;
+			setDataForm({
+				...dataForm,
+				[name]: value,
+			});
+		};
+
      /** Deklrasai Logic Search & Filter **/
-  const _searchTerm = searchTerm.toLowerCase();
+  const _searchTerm = dataForm.searchTerm.toLowerCase();
   const filteredFrameworks = frameworkData.filter((framework) => {
     const matchesSearch =
       framework.name.toLowerCase().includes(_searchTerm) ||
@@ -14,14 +31,14 @@ export default function FrameworkList() {
       framework.details.developer.toLowerCase().includes(_searchTerm)||
        framework.details.releaseYear.toString().includes(_searchTerm);
 
-    const matchesTag = selectedTag ? framework.tags.includes(selectedTag) : true;
+    const matchesTag = dataForm.selectedTag ? framework.tags.includes(dataForm.selectedTag) : true;
 
     return matchesSearch && matchesTag;
   });
 
   /** Deklarasi pengambilan unique tags di frameworkData **/
     const allTags = [
-    ...new Set(frameworkData.flatMap((framework) => framework.tags)),
+    ...new Set(frameworkData.flatMap((framework) => dataForm.selectedTag)),
   ];
 
     return (
@@ -33,13 +50,13 @@ export default function FrameworkList() {
             name="searchTerm"
             placeholder="Search framework..."
             className="w-full p-2 border border-gray-300 rounded mb-4"
-            onChange={(e) => setSearchTerm(e.target.value)}
+            onChange={handleChange}
             />
 
           <select
             name="selectedTag"
             className="w-full p-2 border border-gray-300 rounded mb-4"
-            onChange={(e) => setSelectedTag(e.target.value)}
+            onChange={handleChange}
              >
             <option value="">All Tags</option>
              {allTags.map((tag, index) => (
--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -2,10 +2,13 @@ import { createRoot } from "react-dom/client";
 import FrameworkList from "./FrameworkList";
 import './tailwind.css';
 import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
+import ResponsiveDesign from "./ResponsiveDesign"
+
 
 createRoot(document.getElementById("root"))
     .render(
         <div>
+          <ResponsiveDesign/>  
      <FrameworkListSearchFilter/>
         </div>
     )
